4c7829ffd2735f647989fdefdf15c8c476957747
Dylan Weber <djweber3@en4181828l.fulton.ad.asu.edu>
2020-04-09 13:07:26 -0700

    made changes to how plot times works

--- slime_mold_main_1D.py
+++ slime_mold_main_1D.py
@@ -12,8 +12,8 @@ import pickle
 #I Parameters
 param_dict = {}
 #I.1 Timing/grid/initial positions
-param_dict['N'] = 2 #number of particles for 1D or sqrt of number of partivles for 2D
-param_dict['T'] = .1 #final time
+param_dict['N'] = 10 #number of particles for 1D or sqrt of number of partivles for 2D
+param_dict['T'] = 1 #final time
 param_dict['cell_start'] =  -2.1 #start of spacial grid
 param_dict['cell_end'] = 2.1 #end of spacial grid
 param_dict['h'] = np.divide(param_dict['cell_end'] - param_dict['cell_start'], param_dict['N']) #stepsize in spacial grid
@@ -29,18 +29,23 @@ param_dict['kernal_choice'] = 1 ##choice of interaction kernal 1: polynomial, 2:
 param_dict['M'] = np.vectorize(slime1D.initial_masses)(param_dict['x0'],param_dict['initial_mass_profile']) * param_dict['h'] #particle masses
 param_dict['y0'] = 1 ##food source 1
 param_dict['y1'] = -1 ##food source 2
-param_dict['ABC_params'] = [(1,5,10),(5,1,10)]#,(1,1,10),(10,1,1),(10,1,5),(10,5,1),(1,10,1),(1,10,5),(5,10,1),(1,1,1),(10,1,10),(1,10,10), (10,10,1)] #set of A,B and C values to sweep over
+param_dict['ABC_params'] = [(1,5,10)]#,(5,1,10)]#,(1,1,10),(10,1,1),(10,1,5),(10,5,1),(1,10,1),(1,10,5),(5,10,1),(1,1,1),(10,1,10),(1,10,10), (10,10,1)] #set of A,B and C values to sweep over
 
 #I.3 plotting/saving parameters
-param_dict['plot_times'] = [1]  #times you want to plot a trajectory for
+param_dict['plot_profiles'] = 3  #times you want to plot a trajectory for, will always plot initial and final profile - number is how many additional
 filename = 'test'
 
-def sweep_1D(ABC_parameters, x0, maximum_step_size, T, y0, y1, M, m, eps, kernal_choice, plot_times, file_name):
+def sweep_1D(ABC_parameters, x0, maximum_step_size, T, y0, y1, M, m, eps, kernal_choice, plot_profiles, file_name):
     sol_dict = {}
     for pset in ABC_parameters:
 
         sol = solve_ivp(lambda t,y: slime1D.x_prime(t, y, pset[0], pset[1], pset[2], y0, y1, M, m, eps, kernal_choice), (0, T), x0, method = 'BDF', max_step = maximum_step_size)
-        sol_dict[set] =  sol
+        sol_dict[pset] =  sol
+        steps = len(sol[0, :])
+        steps_between_profiles = steps // plot_profiles
+        plot_times = []
+        plot_times = [i * steps for i in range(0,plot_profiles +1)]
+        plot_times.append(steps - 1)
         slime1D.compute_and_plot_profiles(sol, M, plot_times, param_dict['cell_start'], param_dict['cell_end'], param_dict['eps'], param_dict['y0'], param_dict['y1'], 200, filename + "A%d_B%d_C%d" % (pset[0], pset[1], pset[2]))
 
     return sol_dict
